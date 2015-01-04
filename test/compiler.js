a68ce850dbe3531574bafb718ca36517ec587408
ajlopez <webmaster@ajlopez.com>
2015-01-04 14:47:09 -0300

    Compile integer, string

---
+++ test/compiler.js
@@ -0,0 +1,11 @@
+
+var compiler = require('../lib/compiler');
+
+exports['Compile integer'] = function (test) {
+    test.strictEqual(compiler.compile(42), '42');
+};
+
+exports['Compile string'] = function (test) {
+    test.strictEqual(compiler.compile("foo"), '"foo"');
+};
+
