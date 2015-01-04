37bf99e74d5675f55733a4eb2fec87442dfa58e3
ajlopez <webmaster@ajlopez.com>
2015-01-04 16:18:19 -0300

    Compile list

--- test/compiler.js
+++ test/compiler.js
@@ -1,6 +1,7 @@
 
 var compiler = require('../lib/compiler');
 var symbols = require('../lib/symbol');
+var lists = require('../lib/list');
 
 exports['Compile integer'] = function (test) {
     test.strictEqual(compiler.compile(42), '42');
@@ -20,5 +21,9 @@ exports['Compile symbol in default namespace'] = function (test) {
 
 exports['Compile local symbol'] = function (test) {
     test.equal(compiler.compile(symbols.symbol('x'), { locals: [ 'x', 'y' ] }), 'x');
+};
+
+exports['Compile list'] = function (test) {
+    test.equal(compiler.compile(lists.create([symbols.symbol('list'), 1, 2, 3])), 'cljs.core.list.call(null, 1, 2, 3)');
 };
 
