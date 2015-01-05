1cd63e45105caff33b943ea57a12cc1f283ce91c
ajlopez <webmaster@ajlopez.com>
2015-01-05 19:21:31 -0300

    Create and modify map

--- test/map.js
+++ test/map.js
@@ -9,3 +9,19 @@ exports['Create map'] = function (test) {
     test.equal(map.get("two"), 2);
     test.equal(map.get("three"), 3);
 };
+
+exports['Create and modify map'] = function (test) {
+    var map = maps.create(["one", 1, "two", 2, "three", 3]);
+    
+    var map2 = map.set("three", 4);
+    
+    test.ok(map);
+    test.equal(map.get("one"), 1);
+    test.equal(map.get("two"), 2);
+    test.equal(map.get("three"), 3);
+    
+    test.ok(map2);
+    test.equal(map2.get("one"), 1);
+    test.equal(map2.get("two"), 2);
+    test.equal(map2.get("three"), 4);
+};
