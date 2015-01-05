e0189b2a92cbe3f6bda3c56fe9c422da207c15c8
ajlopez <webmaster@ajlopez.com>
2015-01-05 09:57:22 -0300

    Refactor execute; execute two expressions

--- test/execute.js
+++ test/execute.js
@@ -2,7 +2,23 @@
 var clojjs = require('..');
 
 exports['Execute def'] = function (test) {
+    if (cljs && cljs.core && cljs.core.one)
+        delete cljs.core.one;
+        
     clojjs.execute('(def one 1)');
     test.ok(cljs.core.one);
     test.equal(cljs.core.one, 1);
+};
+
+exports['Execute two defs'] = function (test) {
+    if (cljs && cljs.core && cljs.core.one)
+        delete cljs.core.one;
+    if (cljs && cljs.core && cljs.core.two)
+        delete cljs.core.two;
+        
+    clojjs.execute('(def one 1) (def two 2)');
+    test.ok(cljs.core.one);
+    test.equal(cljs.core.one, 1);
+    test.ok(cljs.core.two);
+    test.equal(cljs.core.two, 2);
 };
