8537b55a164e18a26863ef44e8e9bfee45e4aeaf
ajlopez <webmaster@ajlopez.com>
2015-01-06 06:47:26 -0300

    Map with keywords

--- test/map.js
+++ test/map.js
@@ -1,5 +1,6 @@
 
 var maps = require('../lib/map');
+var keywords = require('../lib/keyword');
 
 exports['Create map'] = function (test) {
     var map = maps.create(["one", 1, "two", 2, "three", 3]);
@@ -8,6 +9,15 @@ exports['Create map'] = function (test) {
     test.equal(map.get("one"), 1);
     test.equal(map.get("two"), 2);
     test.equal(map.get("three"), 3);
+};
+
+exports['Create map with keywords'] = function (test) {
+    var map = maps.create([keywords.keyword("one"), 1, keywords.keyword("two"), 2, keywords.keyword("three"), 3]);
+    
+    test.ok(map);
+    test.equal(map.get(keywords.keyword("one")), 1);
+    test.equal(map.get(keywords.keyword("two")), 2);
+    test.equal(map.get(keywords.keyword("three")), 3);
 };
 
 exports['Create and modify map'] = function (test) {
