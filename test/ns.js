d67915ba8a909a9e795ea11b565ad1a7d04a24fe
ajlopez <webmaster@ajlopez.com>
2015-01-08 07:29:15 -0300

    Namespace to filename

--- test/ns.js
+++ test/ns.js
@@ -1,5 +1,6 @@
 
 var ns = require('../lib/ns');
+var path = require('path');
 
 exports['Namespace is valid'] = function (test) {
     test.equal(ns.isValid('user'), true);
@@ -15,3 +16,10 @@ exports['Namespace is invalid'] = function (test) {
     test.equal(ns.isValid('abC'), false);
     test.equal(ns.isValid('user..core'), false);
 };
+
+exports['To filename'] = function (test) {
+    test.equal(ns.toFilename('./src', 'user.core').replace(/\\/g, "/"), 'src/user/core.cljs');
+    test.equal(ns.toFilename('./src', 'user').replace(/\\/g, "/"), 'src/user.cljs');
+    test.equal(ns.toFilename(path.resolve('./src'), 'user.core'), path.resolve('./src/user/core.cljs'));
+};
+
