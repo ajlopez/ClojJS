15805bdd262a0b6d5a28172bd6e14523463b161d
ajlopez <webmaster@ajlopez.com>
2015-01-09 05:29:00 -0300

    Require namespace once

--- test/require.js
+++ test/require.js
@@ -1,1 +1,51 @@
-var clojjs = require('..');exports['Require namespace'] = function (test) {    if (typeof user != 'undefined' && typeof user.core != 'undefined')        delete user.core;            clojjs.setSource('test/src');    clojjs.requireNamespace('user.core', {});        test.ok(user);    test.ok(user.core);    test.ok(user.core.one);    test.equal(user.core.one, 1);}exports['Cannot resolve namespace'] = function (test) {    if (typeof user != 'undefined' && typeof user.core != 'undefined')        delete user.core;            clojjs.setSource('test/files');        test.throws(        function() {            clojjs.requireNamespace('user.core', {});        },        "Cannot resolve namespace 'user.core'"    );}
+
+var clojjs = require('..');
+
+exports['Require namespace'] = function (test) {
+    if (typeof user != 'undefined' && typeof user.core != 'undefined')
+        delete user.core;
+        
+    clojjs.setSource('test/src');
+    clojjs.requireNamespace('user.core', {});
+    
+    test.ok(user);
+    test.ok(user.core);
+    test.ok(user.core.one);
+    test.equal(user.core.one, 1);
+}
+
+exports['Cannot resolve namespace'] = function (test) {
+    if (typeof user != 'undefined' && typeof user.core != 'undefined')
+        delete user.core;
+        
+    clojjs.setSource('test/files');
+    
+    test.throws(
+        function() {
+            clojjs.requireNamespace('user.core2', {});
+        },
+        "Cannot resolve namespace 'user.core2'"
+    );
+}
+
+exports['Require namespace once'] = function (test) {
+    if (typeof user != 'undefined' && typeof user.core != 'undefined')
+        delete user.core;
+        
+    clojjs.setSource('test/src');
+    clojjs.requireNamespace('user.core', {}, true);
+    
+    test.ok(user);
+    test.ok(user.core);
+    test.ok(user.core.one);
+    test.equal(user.core.one, 1);
+    
+    user.core.one = 2;
+
+    clojjs.requireNamespace('user.core', {});
+    
+    test.ok(user);
+    test.ok(user.core);
+    test.ok(user.core.one);
+    test.equal(user.core.one, 2);
+}
