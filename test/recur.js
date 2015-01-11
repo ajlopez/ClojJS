c088ef48dcade95de2f4a658810ae4e5fdc58ee1
ajlopez <webmaster@ajlopez.com>
2015-01-11 18:28:09 -0300

    recur first tests

---
+++ test/recur.js
@@ -0,0 +1,27 @@
+
+var recurs = require('../lib/recur');
+var lists = require('../lib/list');
+
+exports['Create recur'] = function (test) {
+    var result = recurs.create([1, 2, 3]);
+    
+    test.ok(result);
+    test.equal(result.length(), 3);
+    test.equal(result.get(0), 1);
+    test.equal(result.get(1), 2);
+    test.equal(result.get(2), 3);
+}
+
+exports['Is recur'] = function (test) {
+    var result = recurs.create([1, 2, 3]);
+    
+    test.ok(result);
+    test.ok(recurs.isRecur(result));
+    test.ok(!recurs.isRecur(null));
+    test.ok(!recurs.isRecur(false));
+    test.ok(!recurs.isRecur(true));
+    test.ok(!recurs.isRecur(42));
+    test.ok(!recurs.isRecur("foo"));
+    test.ok(!recurs.isRecur(lists.create([1, 2, 3])));
+}
+
