c73a19b7f9459ff8b472c33c254456b3bc5110dc
ajlopez <webmaster@ajlopez.com>
2015-01-13 05:34:57 -0300

    Lazy seq with first test

---
+++ test/lazyseq.js
@@ -0,0 +1,24 @@
+
+var lazyseqs = require('../lib/lazyseq');
+var lists = require('../lib/list');
+
+exports['create and consume lazy seq'] = function (test) {
+    var n = 1;
+    
+    var fn = function () {
+        return lists.list(n++, lazyseqs.create(fn));
+    }
+    
+    var lseq = lazyseqs.create(fn);
+    
+    test.ok(lseq);
+    
+    test.equal(lseq.first(), 1);
+    test.equal(lseq.first(), 1);
+    test.ok(lseq.next());
+    test.equal(lseq.next().first(), 2);
+    test.equal(lseq.next().first(), 2);
+    test.ok(lseq.next());
+    test.equal(lseq.next().next().first(), 3);
+    test.equal(lseq.next().next().first(), 3);
+}
