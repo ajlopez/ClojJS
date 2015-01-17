cb70134893a9deaa8f2f8658e9e0776e6cd5e555
ajlopez <webmaster@ajlopez.com>
2015-01-17 07:08:05 -0300

    Keyword equals

--- test/keyword.js
+++ test/keyword.js
@@ -20,3 +20,18 @@ exports['Is keyword'] = function (test) {
     test.ok(!keywords.isKeyword('foo'));
 };
 
+exports['Equals'] = function (test) {
+    var keyword = keywords.keyword('foo');
+    var keyword2 = keywords.keyword('foo');
+    var keyword3 = keywords.keyword('bar');
+    
+    test.ok(keyword.equals(keyword2));
+    test.ok(keyword2.equals(keyword));
+    test.ok(!keyword.equals(keyword3));
+    test.ok(!keyword3.equals(keyword));
+    
+    test.ok(!keyword.equals(null));
+    test.ok(!keyword.equals(42));
+    test.ok(!keyword.equals("foo"));
+    test.ok(!keyword.equals(":foo"));
+};
