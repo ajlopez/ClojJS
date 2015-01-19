c4cb62ab8f47d44e3ca9121be9bdf1598bd63214
ajlopez <webmaster@ajlopez.com>
2015-01-19 08:30:41 -0300

    assoc in core.cljs with map is tests

--- src/core.cljs
+++ src/core.cljs
@@ -199,3 +199,9 @@ called."
         (when (pos? n) 
             (when-let [s (seq coll)]
                 (cons (first s) (take (dec n) (rest s)))))))
+                
+(defn assoc
+    [map & keyvalues]
+    (.assoc map ((to-array keyvalues))))
+    
+
