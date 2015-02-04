e31ad20bc3706ade6849f8e5a0bf77aa560f0840
ajlopez <webmaster@ajlopez.com>
2015-02-04 08:03:05 -0300

    Additional compile file tests

--- test/compilefile.js
+++ test/compilefile.js
@@ -13,3 +13,27 @@ exports['Compile integer to file'] = function (test) {
     var result = fs.readFileSync(targetfile).toString();
     test.strictEqual(result, '42\n');
 };
+
+exports['Compile string'] = function (test) {
+    var sourcefile = path.join(__dirname, 'files', 'string.cljs');
+    var targetfile = path.join(__dirname, 'output', 'string.js');
+    clojjs.compileFile(sourcefile, targetfile);
+    var result = fs.readFileSync(targetfile).toString();
+    test.strictEqual(result, '"foo"\n');
+};
+
+exports['Compile nil'] = function (test) {
+    var sourcefile = path.join(__dirname, 'files', 'nil.cljs');
+    var targetfile = path.join(__dirname, 'output', 'nil.js');
+    clojjs.compileFile(sourcefile, targetfile);
+    var result = fs.readFileSync(targetfile).toString();
+    test.strictEqual(result, 'null\n');
+};
+
+exports['Compile symbol in default namespace'] = function (test) {
+    var sourcefile = path.join(__dirname, 'files', 'symbol.cljs');
+    var targetfile = path.join(__dirname, 'output', 'symbol.js');
+    clojjs.compileFile(sourcefile, targetfile);
+    var result = fs.readFileSync(targetfile).toString();
+    test.equal(result, 'cljs.core.first\n');
+};
