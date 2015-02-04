77b2fe810f7301833bddcad15f155b54f12caf78
ajlopez <webmaster@ajlopez.com>
2015-02-04 08:06:22 -0300

    Refactor compile file tests

--- test/compilefile.js
+++ test/compilefile.js
@@ -6,34 +6,26 @@ var parsers = require('../lib/parser');
 var path = require('path');
 var fs = require('fs');
 
-exports['Compile integer to file'] = function (test) {
-    var sourcefile = path.join(__dirname, 'files', 'integer.cljs');
-    var targetfile = path.join(__dirname, 'output', 'integer.js');
+function compile(name, expected, test) {
+    var sourcefile = path.join(__dirname, 'files', name + '.cljs');
+    var targetfile = path.join(__dirname, 'output', name + '.js');
     clojjs.compileFile(sourcefile, targetfile);
     var result = fs.readFileSync(targetfile).toString();
-    test.strictEqual(result, '42\n');
+    test.strictEqual(result, expected + '\n');
+}
+
+exports['Compile integer to file'] = function (test) {
+    compile('integer', '42', test);
 };
 
 exports['Compile string'] = function (test) {
-    var sourcefile = path.join(__dirname, 'files', 'string.cljs');
-    var targetfile = path.join(__dirname, 'output', 'string.js');
-    clojjs.compileFile(sourcefile, targetfile);
-    var result = fs.readFileSync(targetfile).toString();
-    test.strictEqual(result, '"foo"\n');
+    compile('string', '"foo"', test);
 };
 
 exports['Compile nil'] = function (test) {
-    var sourcefile = path.join(__dirname, 'files', 'nil.cljs');
-    var targetfile = path.join(__dirname, 'output', 'nil.js');
-    clojjs.compileFile(sourcefile, targetfile);
-    var result = fs.readFileSync(targetfile).toString();
-    test.strictEqual(result, 'null\n');
+    compile('nil', 'null', test);
 };
 
 exports['Compile symbol in default namespace'] = function (test) {
-    var sourcefile = path.join(__dirname, 'files', 'symbol.cljs');
-    var targetfile = path.join(__dirname, 'output', 'symbol.js');
-    clojjs.compileFile(sourcefile, targetfile);
-    var result = fs.readFileSync(targetfile).toString();
-    test.equal(result, 'cljs.core.first\n');
+    compile('symbol', 'cljs.core.first', test);
 };
