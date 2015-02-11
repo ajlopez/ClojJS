6ce9309b1b3182c6937a7fb0c1e085b7c0f5b4fe
ajlopez <webmaster@ajlopez.com>
2015-02-11 09:32:20 -0300

    Execute require using provided clojjs.json

--- test/project.js
+++ test/project.js
@@ -3,8 +3,23 @@ var clojjs = require('..');
 var path = require('path');
 
 exports['Load simple project'] = function (test) {
+    if (typeof myapp != 'undefined')
+        delete myapp;
+        
     clojjs.loadConfiguration(path.join(__dirname, 'projects', 'simple', 'clojjs.json'));
     clojjs.requireNamespace('myapp.core', {});
+    test.ok(myapp);
+    test.ok(myapp.core);
+    test.ok(myapp.core.answer);
+    test.equal(myapp.core.answer, 42);
+}
+
+exports['Execute require and load simple project'] = function (test) {
+    if (typeof myapp != 'undefined')
+        delete myapp;
+        
+    clojjs.loadConfiguration(path.join(__dirname, 'projects', 'simple', 'clojjs.json'));
+    clojjs.execute("(require 'myapp.core true)", {});
     test.ok(myapp);
     test.ok(myapp.core);
     test.ok(myapp.core.answer);
