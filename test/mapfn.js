49ede38c2a2e1ea4dd6aabe881ec4a331c5b758e
ajlopez <webmaster@ajlopez.com>
2020-07-02 12:21:32 -0300

    mapfn tests refactor

--- test/mapfn.js
+++ test/mapfn.js
@@ -1,32 +1,33 @@
 
-var mapfn = require('../lib/mapfn');
-var lists = require('../lib/list');
-var vectors = require('../lib/vector');
+const mapfn = require('../lib/mapfn');
+const lists = require('../lib/list');
+const vectors = require('../lib/vector');
 
 exports['Evaluate map on list'] = function (test) {
-    var result = mapfn.apply(function (x) { return x+1; }, lists.create([1, 2, 3]));
+    const result = mapfn.apply(function (x) { return x+1; }, lists.create([1, 2, 3]));
     
     test.ok(result);
     test.equal(result.asString(), '(2 3 4)');
 }
 
 exports['Evaluate map on two lists'] = function (test) {
-    var result = mapfn.apply(function (x, y) { return x + y; }, lists.create([1, 2, 3]), lists.create([4, 5, 6]));
+    const result = mapfn.apply(function (x, y) { return x + y; }, lists.create([1, 2, 3]), lists.create([4, 5, 6]));
     
     test.ok(result);
     test.equal(result.asString(), '(5 7 9)');
 }
 
 exports['Evaluate map on two lists of different sizes'] = function (test) {
-    var result = mapfn.apply(function (x, y) { return x + y; }, lists.create([1, 2, 3]), lists.create([4, 5, 6, 7, 8, 9]));
+    const result = mapfn.apply(function (x, y) { return x + y; }, lists.create([1, 2, 3]), lists.create([4, 5, 6, 7, 8, 9]));
     
     test.ok(result);
     test.equal(result.asString(), '(5 7 9)');
 }
 
 exports['Evaluate map on vector'] = function (test) {
-    var result = mapfn.apply(function (x) { return x+1; }, vectors.create([1, 2, 3]));
+    const result = mapfn.apply(function (x) { return x+1; }, vectors.create([1, 2, 3]));
     
     test.ok(result);
     test.equal(result.asString(), '(2 3 4)');
 }
+
