0b2f717917903a8006c411744c1c0be223de3962
ajlopez <webmaster@ajlopez.com>
2020-07-08 08:34:50 -0300

    Compile file test refactor

--- test/compilefile.js
+++ test/compilefile.js
@@ -1,16 +1,19 @@
 
-var clojjs = require('..');
-var symbols = require('../lib/symbol');
-var lists = require('../lib/list');
-var parsers = require('../lib/parser');
-var path = require('path');
-var fs = require('fs');
+const clojjs = require('..');
+const symbols = require('../lib/symbol');
+const lists = require('../lib/list');
+const parsers = require('../lib/parser');
+const path = require('path');
+const fs = require('fs');
 
 function compile(name, expected, test, contained) {
-    var sourcefile = path.join(__dirname, 'files', name + '.cljs');
-    var targetfile = path.join(__dirname, 'output', name + '.js');
+    const sourcefile = path.join(__dirname, 'files', name + '.cljs');
+    const targetfile = path.join(__dirname, 'output', name + '.js');
+    
     clojjs.compileFile(sourcefile, targetfile);
-    var result = fs.readFileSync(targetfile).toString();
+    
+    const result = fs.readFileSync(targetfile).toString();
+    
     if (contained)
         test.ok(result.indexOf(expected) >= 0);
     else
@@ -44,6 +47,7 @@ exports['Compile def in default namespace'] = function (test) {
 exports['Compile def in current namespace'] = function (test) {
     if (typeof user != 'undefined')
         delete user;
+    
     compile('nsdefone', 'user.one = 1', test, true);
     test.ok(user);
     test.ok(user.one);
