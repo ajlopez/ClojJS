4914333eab63a6c851a3aa414b6aa9f7747e5265
ajlopez <webmaster@ajlopez.com>
2020-07-14 11:09:22 -0300

    is test minor refactor

--- test/is.js
+++ test/is.js
@@ -1,12 +1,14 @@
 
-var clojjs = require('..');
-var fs = require('fs');
-var path = require('path');
+const clojjs = require('..');
+const fs = require('fs');
+const path = require('path');
 
 exports['Execute is files'] = function (test) {
-    var filenames = fs.readdirSync(path.join(__dirname, 'is'));
+    const filenames = fs.readdirSync(path.join(__dirname, 'is'));
+    
     filenames.forEach(function (filename) {
         console.log();
         clojjs.executeFile(path.join(__dirname, 'is', filename));
     });
 };
+
