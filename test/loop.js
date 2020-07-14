62c3dca251efed7a83839c52b0058d6ca23cbb8f
ajlopez <webmaster@ajlopez.com>
2020-07-14 11:10:21 -0300

    loop test minor refactor

--- test/loop.js
+++ test/loop.js
@@ -1,9 +1,9 @@
 
-var loops = require('../lib/loop');
-var recurs = require('../lib/recur');
+const loops = require('../lib/loop');
+const recurs = require('../lib/recur');
 
 exports['Evaluate loop'] = function (test) {
-    var fn = function (x) {
+    const fn = function (x) {
         return function (y) {
             if (y > 0)
                 return recurs.create([x + y, y - 1]);
@@ -12,8 +12,9 @@ exports['Evaluate loop'] = function (test) {
         }
     };
     
-    var result = loops.evaluate(fn, [0, 3]);
+    const result = loops.evaluate(fn, [0, 3]);
     
     test.ok(result);
     test.equal(result, 1 + 2 + 3);
 }
+
